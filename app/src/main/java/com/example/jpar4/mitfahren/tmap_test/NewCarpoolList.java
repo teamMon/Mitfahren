6e8c2c85f7ea8cc0f24e18b9bf5bbd12383b1bf7
jpar4 <jpar424@gmail.com>
2017-09-17 11:16:45 +0900

    개발3주차 일요일

---
+++ app/src/main/java/com/example/jpar4/mitfahren/tmap_test/NewCarpoolList.java
@@ -0,0 +1,15 @@
+package com.example.jpar4.mitfahren.tmap_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jpar4.mitfahren.R;
+
+public class NewCarpoolList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_carpool_list_activity);
+    }
+}
